642750f02500bed364208c06e833b7a33ed988a8
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-12-22 01:57:41 +0300

    добавляет проект в список проектов

--- source/js/modules/animation/init-animate-batch.js
+++ source/js/modules/animation/init-animate-batch.js
@@ -1,15 +1,15 @@
 const initAnimateBatch = () => {
 
-  gsap.set('[data-animate="fade"]', {opacity: 0});
+  // gsap.set('[data-animate="fade"]', {opacity: 0});
   gsap.set('[data-animate="fade-in"]', {opacity: 0, y: 25});
   gsap.set('[data-animate="fade-scale"]', {opacity: 0, scale: 0});
 
-  ScrollTrigger.batch('[data-animate="fade"]', {
-    onEnter: (batch) => gsap.to(batch, {opacity: 1, stagger: {each: 0.25}}),
-    start: '90% 90%',
-    end: 'top top',
-    scrub: true,
-  });
+  // ScrollTrigger.batch('[data-animate="fade"]', {
+  //   onEnter: (batch) => gsap.to(batch, {opacity: 1, stagger: {each: 0.25}}),
+  //   start: '90% 90%',
+  //   end: 'top top',
+  //   scrub: true,
+  // });
 
   ScrollTrigger.batch('[data-animate="fade-in"]', {
     onEnter: (batch) => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.25}}),
